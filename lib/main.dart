4eb88cbb14915da29d694589cdc17e0007409d01
FlyPenguinFly33 <58951436+FlyPenguinFly33@users.noreply.github.com>
2020-07-26 21:42:20 +0300

    Added Charts

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import "./widgets/new_transaction.dart";
 import "./models/transaction.dart";
 import "./widgets/transaction_list.dart";
+import "./widgets/chart.dart";
 
 void main() {
   runApp(MyApp());
@@ -70,6 +71,13 @@ class _MyHomePageState extends State<MyHomePage> {
     // ),
   ];
 
+  List<Transaction> get _recentTransactions {
+    return _userTransactions
+      ..where((tx) {
+        return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+      }).toList();
+  }
+
   void _addNewTransaction(String title, double amount) {
     final newTX = Transaction(
       title: title,
@@ -120,17 +128,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                child: Text(
-                  "*CHART*",
-                  textAlign: TextAlign.center,
-                ),
-                color: Theme.of(context).primaryColorDark,
-                elevation: 5,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(transactions: _userTransactions),
           ],
         ),
